1852f983e62df8ef0aa91d5c9b9000d4f1bfda42
barbadmb <66271969+barbadmb@users.noreply.github.com>
2020-06-06 10:11:50 +0430

    update deposetry

---
+++ Xenhtml/data/Library/Application Support/Xen HTML/Setup UI/precache-manifest.b196cf6eed2ae97754b3e310c3c72001.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be9b711989ccfb7da41",
+    "url": "css/app.7499817f.css"
+  },
+  {
+    "revision": "34a055f57e13c83eba89c35dbc169e83",
+    "url": "index.html"
+  },
+  {
+    "revision": "5be9b711989ccfb7da41",
+    "url": "js/app.bdbb8dec.js"
+  },
+  {
+    "revision": "9da6be9b0ccaabd5b26d",
+    "url": "js/chunk-vendors.f3485ebd.js"
+  },
+  {
+    "revision": "b2a457bb622fe4a4f2bde0e44086405e",
+    "url": "manifest.json"
+  }
+]);
